b0f3b8911618384b87ab6994a5add6d7ccd9f86c
Joe-noh <goflb.jh@gmail.com>
2019-02-25 04:51:07 +0900

    AccessTokenでencode, decode

---
+++ test/lild_web/access_token_test.exs
@@ -0,0 +1,15 @@
+defmodule LILDWeb.AccessTokenTest do
+  use ExUnit.Case, async: true
+
+  alias LILDWeb.AccessToken
+
+  test "payload" do
+    {:ok, jwt, payload} = AccessToken.encode(%{id: "12345"})
+    {:ok, ^payload} = AccessToken.decode(jwt)
+
+    assert payload["sub"] == "12345"
+    assert payload["aud"] == "User"
+    assert payload["iss"] == "LILD"
+    assert payload["exp"] - payload["iat"] == 24 * 60 * 60
+  end
+end
