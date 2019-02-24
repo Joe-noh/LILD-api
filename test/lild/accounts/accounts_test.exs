cae4d8da385ab610d35b3f494bdab610691bd270
Joe-noh <goflb.jh@gmail.com>
2019-02-25 01:23:01 +0900

    contextのテストもfixture使うようにしてく

--- test/lild/accounts/accounts_test.exs
+++ test/lild/accounts/accounts_test.exs
@@ -1,71 +1,52 @@
 defmodule LILD.AccountsTest do
   use LILD.DataCase
 
-  alias LILD.Repo
   alias LILD.Accounts
 
   describe "users" do
-    alias LILD.Accounts.User
-    alias LILD.Accounts.FirebaseAccount
+    setup [:create_user]
 
-    @valid_attrs %{avatar_url: "some avatar_url", name: "some name"}
-    @update_attrs %{avatar_url: "some updated avatar_url", name: "some updated name"}
-    @invalid_attrs %{avatar_url: nil, name: nil}
-
-    def user_fixture(attrs \\ %{}) do
-      {:ok, user} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_user()
-
-      user
-    end
-
-    test "user and firebase_account" do
-      user = user_fixture()
-
-      user
-      |> Ecto.build_assoc(:firebase_account)
-      |> FirebaseAccount.changeset(%{provider: "some provider", uid: "some uid"})
-      |> IO.inspect()
-      |> Repo.insert!()
-      |> Ecto.assoc(:user)
-      |> Repo.one()
-      |> IO.inspect()
-    end
-
-    test "get_user!/1 returns the user with given id" do
-      user = user_fixture()
+    test "get_user!/1 returns the user with given id", %{user: user} do
       assert Accounts.get_user!(user.id) == user
     end
 
     test "create_user/1 with valid data creates a user" do
-      assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
-      assert user.avatar_url == "some avatar_url"
-      assert user.name == "some name"
+      user_attrs = Fixture.Accounts.user()
+      firebase_account_attrs = Fixture.Accounts.firebase_account()
+      {:ok, %{user: user, firebase_account: firebase_account}} = Accounts.create_user(user_attrs, firebase_account_attrs)
+
+      assert user.name == user_attrs.name
+      assert user.avatar_url == user_attrs.avatar_url
+      assert firebase_account.uid == firebase_account_attrs.uid
+      assert firebase_account.provider == firebase_account_attrs.provider
     end
 
     test "create_user/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
+      assert {:error, _, %Ecto.Changeset{}, _} = Accounts.create_user(%{}, %{})
     end
 
-    test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
-      assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
-      assert user.avatar_url == "some updated avatar_url"
-      assert user.name == "some updated name"
+    test "update_user/2 with valid data updates the user", %{user: user} do
+      update_attrs = Fixture.Accounts.user()
+
+      assert {:ok, user} = Accounts.update_user(user, update_attrs)
+      assert user.name == update_attrs.name
+      assert user.avatar_url == update_attrs.avatar_url
     end
 
-    test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture()
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
+    test "update_user/2 with invalid data returns error changeset", %{user: user} do
+      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, %{name: ''})
       assert user == Accounts.get_user!(user.id)
     end
 
-    test "delete_user/1 deletes the user" do
-      user = user_fixture()
-      assert {:ok, %User{}} = Accounts.delete_user(user)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
+    test "delete_user/1 deletes the user", %{user: user} do
+      assert {:ok, %{user: _, firebase_account: _}} = Accounts.delete_user(user)
+      assert_raise Ecto.NoResultsError, fn ->
+        Accounts.get_user!(user.id)
+      end
     end
+  end
+
+  defp create_user(_) do
+    Accounts.create_user(Fixture.Accounts.user(), Fixture.Accounts.firebase_account())
   end
 end
