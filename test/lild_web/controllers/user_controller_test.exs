bf3a83b967b498805856c5191e5e89ff87b06a8f
Joe-noh <goflb.jh@gmail.com>
2019-02-25 00:37:47 +0900

    テストデータの扱いがイマイチなのでmoduleにまとめる

--- test/lild_web/controllers/user_controller_test.exs
+++ test/lild_web/controllers/user_controller_test.exs
@@ -6,37 +6,9 @@ defmodule LILDWeb.UserControllerTest do
   alias LILD.Accounts
   alias LILD.Accounts.User
 
-  @create_attrs %{
-    avatar_url: "some avatar_url",
-    name: "some name",
-    id_token: "firebase.id_token"
-  }
-  @update_attrs %{
-    avatar_url: "some updated avatar_url",
-    name: "some updated name"
-  }
   @invalid_attrs %{avatar_url: nil, name: nil, id_token: nil}
 
-  @firebase_response %{
-    "aud" => "lild-dev",
-    "auth_time" => 1_550_966_987,
-    "exp" => 1_550_970_587,
-    "firebase" => %{
-      "identities" => %{"twitter.com" => ["872977718727458816"]},
-      "sign_in_provider" => "twitter.com"
-    },
-    "iat" => 1_550_966_987,
-    "iss" => "https://securetoken.google.com/lild-dev",
-    "name" => "paintgraphics",
-    "picture" => "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
-    "sub" => "tR7hx0eEqeZYYOM9mODVmM39TUP2",
-    "user_id" => "tR7hx0eEqeZYYOM9mODVmM39TUP2"
-  }
-
-  def fixture(:user) do
-    {:ok, user} = Accounts.create_user(@create_attrs)
-    user
-  end
+  @firebase_response Fixture.Accounts.firebase_id_token_payload
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -44,16 +16,15 @@ defmodule LILDWeb.UserControllerTest do
 
   describe "create user" do
     test_with_mock "renders user when data is valid", %{conn: conn}, Accounts, [:passthrough], verify_id_token: fn _ -> {:ok, @firebase_response} end do
-      conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
-      assert %{"id" => id} = json_response(conn, 201)["data"]
+      params = Fixture.Accounts.user(%{id_token: "firebase.id_token"})
+      data = conn
+        |> post(Routes.user_path(conn, :create), user: params)
+        |> json_response(201)
+        |> Map.get("data")
 
-      conn = get(conn, Routes.user_path(conn, :show, id))
-
-      assert %{
-               "id" => id,
-               "avatar_url" => "some avatar_url",
-               "name" => "some name"
-             } = json_response(conn, 200)["data"]
+      assert data["id"] |> is_binary
+      assert data["name"] == params[:name]
+      assert data["avatar_url"] == params[:avatar_url]
     end
 
     test_with_mock "renders errors when data is invalid", %{conn: conn}, Accounts, [:passthrough], verify_id_token: fn _ -> {:ok, @firebase_response} end do
@@ -65,17 +36,15 @@ defmodule LILDWeb.UserControllerTest do
   describe "update user" do
     setup [:create_user]
 
-    test "renders user when data is valid", %{conn: conn, user: %User{id: id} = user} do
-      conn = put(conn, Routes.user_path(conn, :update, user), user: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+    test "renders user when data is valid", %{conn: conn, user: user} do
+      params = Fixture.Accounts.user
+      data = conn
+        |> put(Routes.user_path(conn, :update, user), user: params)
+        |> json_response(200)
 
-      conn = get(conn, Routes.user_path(conn, :show, id))
-
-      assert %{
-               "id" => id,
-               "avatar_url" => "some updated avatar_url",
-               "name" => "some updated name"
-             } = json_response(conn, 200)["data"]
+      assert data["id"] |> is_binary
+      assert data["name"] == params[:name]
+      assert data["avatar_url"] == params[:avatar_url]
     end
 
     test "renders errors when data is invalid", %{conn: conn, user: user} do
@@ -98,7 +67,6 @@ defmodule LILDWeb.UserControllerTest do
   end
 
   defp create_user(_) do
-    user = fixture(:user)
-    {:ok, user: user}
+    Accounts.create_user(Fixture.Accounts.user, Fixture.Accounts.firebase_account)
   end
 end
