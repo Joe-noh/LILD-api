eed845853479ba49fb43671e99d488c7ebe73439
Joe-noh <goflb.jh@gmail.com>
2019-02-25 00:50:13 +0900

    テストでmodule attr使うのあんま好きじゃない

--- lib/lild_web/controllers/user_controller.ex
+++ lib/lild_web/controllers/user_controller.ex
@@ -6,8 +6,8 @@ defmodule LILDWeb.UserController do
 
   action_fallback LILDWeb.FallbackController
 
-  def create(conn, %{"user" => user_params = %{"id_token" => id_token}}) do
-    with {:ok, payload} <- Accounts.verify_id_token(id_token),
+  def create(conn, %{"user" => user_params}) do
+    with {:ok, payload} <- Accounts.verify_id_token(Map.get(user_params, "id_token")),
          %{"firebase" => %{"sign_in_provider" => provider}, "user_id" => uid} = payload,
          firebase_account_params = %{provider: provider, uid: uid},
          {:ok, %{user: %User{} = user}} <- Accounts.create_user(user_params, firebase_account_params) do
@@ -36,7 +36,7 @@ defmodule LILDWeb.UserController do
   def delete(conn, %{"id" => id}) do
     user = Accounts.get_user!(id)
 
-    with {:ok, %User{}} <- Accounts.delete_user(user) do
+    with {:ok, _} <- Accounts.delete_user(user) do
       send_resp(conn, :no_content, "")
     end
   end
