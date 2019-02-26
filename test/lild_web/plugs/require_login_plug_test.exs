7084ff8d7c868218e6304080f414245ac48c36f4
Joe-noh <goflb.jh@gmail.com>
2019-02-27 01:12:21 +0900

    current_userがないときは401を返すplug

---
+++ test/lild_web/plugs/require_login_plug_test.exs
@@ -0,0 +1,32 @@
+defmodule LILDWeb.RequireLoginPlugTest do
+  use LILDWeb.ConnCase
+
+  alias LILD.Accounts
+  alias LILDWeb.RequireLoginPlug
+
+  @state RequireLoginPlug.init([])
+
+  describe "logged in" do
+    setup [:create_user]
+
+    test "pass when logged in", %{conn: conn, user: user} do
+      conn = Plug.Conn.assign(conn, :current_user, user) |> RequireLoginPlug.call(@state)
+
+      assert conn.status == nil
+      assert conn.halted == false
+    end
+
+    defp create_user(_) do
+      Accounts.create_user(Fixture.Accounts.user(), Fixture.Accounts.firebase_account())
+    end
+  end
+
+  describe "not logged in" do
+    test "401 when not logged in", %{conn: conn} do
+      conn = RequireLoginPlug.call(conn, @state)
+
+      assert conn.status == 401
+      assert conn.halted == true
+    end
+  end
+end
