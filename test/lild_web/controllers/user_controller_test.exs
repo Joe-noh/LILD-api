68a1b9b67590b77f4583f8caef28d9f1b5527c08
Joe-noh <goflb.jh@gmail.com>
2019-02-27 01:35:16 +0900

    ログインしないとできないことをテスト

--- test/lild_web/controllers/user_controller_test.exs
+++ test/lild_web/controllers/user_controller_test.exs
@@ -7,7 +7,7 @@ defmodule LILDWeb.UserControllerTest do
   @firebase_response Fixture.Accounts.firebase_id_token_payload()
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    %{conn: put_req_header(conn, "accept", "application/json")}
   end
 
   describe "create user" do
@@ -35,7 +35,7 @@ defmodule LILDWeb.UserControllerTest do
   end
 
   describe "update user" do
-    setup [:create_user]
+    setup [:create_user, :login]
 
     test "renders user when data is valid", %{conn: conn, user: user} do
       params = Fixture.Accounts.user()
@@ -58,10 +58,20 @@ defmodule LILDWeb.UserControllerTest do
 
       assert errors != %{}
     end
+
+    test "401 when not logged in", %{conn: conn, user: user} do
+      errors =
+        Plug.Conn.assign(conn, :current_user, nil)
+        |> put(Routes.user_path(conn, :update, user))
+        |> json_response(401)
+        |> Map.get("errors")
+
+      assert errors != %{}
+    end
   end
 
   describe "delete user" do
-    setup [:create_user]
+    setup [:create_user, :login]
 
     test "deletes chosen user", %{conn: conn, user: user} do
       assert delete(conn, Routes.user_path(conn, :delete, user)) |> response(204)
@@ -70,9 +80,23 @@ defmodule LILDWeb.UserControllerTest do
         get(conn, Routes.user_path(conn, :show, user))
       end
     end
+
+    test "401 when not logged in", %{conn: conn, user: user} do
+      errors =
+        Plug.Conn.assign(conn, :current_user, nil)
+        |> delete(Routes.user_path(conn, :delete, user))
+        |> json_response(401)
+        |> Map.get("errors")
+
+      assert errors != %{}
+    end
   end
 
   defp create_user(_) do
     Accounts.create_user(Fixture.Accounts.user(), Fixture.Accounts.firebase_account())
+  end
+
+  defp login(%{conn: conn, user: user}) do
+    %{conn: Plug.Conn.assign(conn, :current_user, user)}
   end
 end
