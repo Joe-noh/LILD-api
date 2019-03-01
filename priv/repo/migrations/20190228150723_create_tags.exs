cd645eb13ce970a63dda2ad69d0a87846ff1d239
Joe-noh <goflb.jh@gmail.com>
2019-03-01 22:22:55 +0900

    やっぱ外部キー制約必要

--- priv/repo/migrations/20190228150723_create_tags.exs
+++ priv/repo/migrations/20190228150723_create_tags.exs
@@ -12,11 +12,8 @@ defmodule LILD.Repo.Migrations.CreateTags do
     create unique_index(:tags, [:name], name: :tags_name_index)
 
     create table(:dreams_tags, primary_key: false) do
-      add :dream_id, :binary_id, null: false
-      add :tag_id, :binary_id, null: false
+      add :dream_id, references(:dreams, on_delete: :delete_all, type: :binary_id), null: false
+      add :tag_id, references(:tags, on_delete: :delete_all, type: :binary_id), null: false
     end
-
-    create index(:dreams_tags, [:dream_id])
-    create index(:dreams_tags, [:tag_id])
   end
 end
