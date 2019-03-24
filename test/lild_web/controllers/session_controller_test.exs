10be494a8cd2fc7e533f74cceb1e6c80dc867d04
Joe-noh <goflb.jh@gmail.com>
2019-03-24 14:36:25 +0900

    session#updateのテスト

--- test/lild_web/controllers/session_controller_test.exs
+++ test/lild_web/controllers/session_controller_test.exs
@@ -1,28 +1,30 @@
 defmodule LILDWeb.SessionControllerTest do
   use LILDWeb.ConnCase, async: true
 
-  import Mock
-  alias LILD.Accounts
+  alias LILDWeb.RefreshToken
 
-  describe "create" do
+  describe "update" do
     setup [:create_user]
 
-    test_with_mock "ログインする", %{conn: conn, user: user, provider: provider, uid: uid}, Accounts, [:passthrough],
-      verify_id_token: fn _ -> {:ok, %{"provider" => provider, "uid" => uid}} end do
+    test "refresh tokenとaccess tokenを交換する", %{conn: conn, user: user} do
+      {:ok, refresh_token, _payload} = RefreshToken.encode(user)
       json =
-        post(conn, Routes.session_path(conn, :create), %{firebase: Fixture.Accounts.firebase()})
-        |> json_response(201)
+        put(conn, Routes.session_path(conn, :update), %{refresh_token: refresh_token})
+        |> json_response(200)
 
       assert json["user"]["id"] |> is_binary
       assert json["user"]["name"] == user.name
       assert json["user"]["avatar_url"] == user.avatar_url
-      assert json["auth"]["token"] |> is_binary
+      assert json["auth"]["access_token"] |> is_binary
+      assert json["auth"]["refresh_token"] |> is_binary
     end
 
-    test_with_mock "SocialAccountが見つからないときは401", %{conn: conn, provider: provider, uid: uid}, Accounts, [:passthrough],
-      verify_id_token: fn _ -> {:ok, %{"provider" => provider, "uid" => uid <> "1"}} end do
+    test "refresh tokenが期限切れの場合は401", %{conn: conn, user: user} do
+      while_ago = DateTime.utc_now |> DateTime.add(-10, :second) |> DateTime.to_unix
+      {:ok, refresh_token, _payload} = RefreshToken.generate_and_sign(%{"exp" => while_ago, "sub" => user.id})
+
       errors =
-        post(conn, Routes.session_path(conn, :create), firebase: Fixture.Accounts.firebase())
+        put(conn, Routes.session_path(conn, :update), %{refresh_token: refresh_token})
         |> json_response(401)
         |> Map.get("errors")
 
@@ -32,7 +34,7 @@ defmodule LILDWeb.SessionControllerTest do
 
   defp create_user(_) do
     social_params = %{"provider" => provider, "uid" => uid} = Fixture.Accounts.social_account()
-    {:ok, %{user: user}} = Accounts.create_user(Fixture.Accounts.user(), social_params)
+    {:ok, %{user: user}} = LILD.Accounts.create_user(Fixture.Accounts.user(), social_params)
 
     %{user: user, provider: provider, uid: uid}
   end
